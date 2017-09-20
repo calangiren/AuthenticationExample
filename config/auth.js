7900ef1f59699517b70206bf9b2510a06ffe4f90
Sunny C <sunmaloto@outlook.com>
2017-09-20 20:59:59 +1200

    First commit

--- config/auth.js
+++ config/auth.js
@@ -4,8 +4,8 @@
 module.exports = {
 
     'facebookAuth' : {
-        'clientID'        : 'your-secret-clientID-here', // your App ID
-        'clientSecret'    : 'your-client-secret-here', // your App Secret
+        'clientID'        : '1432746160096411', // your App ID
+        'clientSecret'    : '6e84f8e11b9c4fe6cdf8a0caee98e698', // your App Secret
         'callbackURL'     : 'http://localhost:8080/auth/facebook/callback',
         'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email'
 
@@ -18,8 +18,8 @@ module.exports = {
     },
 
     'googleAuth' : {
-        'clientID'         : 'your-secret-clientID-here',
-        'clientSecret'     : 'your-client-secret-here',
+        'clientID'         : '924699244243-gsuit103ri9ohbl87t972nb7uu7skaco.apps.googleusercontent.com',
+        'clientSecret'     : 'zKfn6zNEXSlnBqR0MbD2VG-e',
         'callbackURL'      : 'http://localhost:8080/auth/google/callback'
     }
 
